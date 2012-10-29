11f2e79b531eec1f221c4e7f5d1fbeebae8ff0cb
Jiayi Sandy Wu <jiayisandywu@gmail.com>
2012-10-28 22:11:56 -0400

    Added back in invite Friends button

--- server/static/js/facebook.js
+++ server/static/js/facebook.js
@@ -132,10 +132,24 @@ function($, __, FB) {
     });
   };
 
+  var showSendDialogProfile = function(cb) {
+    // TODO(Sandy): Don't hardcode link?
+    var sendDialogLink = 'http://uwflow.com';
+
+    FB.ui({
+        method: 'send',
+        name: 'Flow',
+        link: sendDialogLink,
+        picture: 'http://uwflow.com/static/img/logo/flow_75x75.png',
+        description: 'Plan your course with friends in mind!'
+      }, cb);
+  }
+
   return {
+    initConnectButton: initConnectButton,
     loginIfPossible: loginIfPossible,
     logout: logout,
-    initConnectButton: initConnectButton
+    showSendDialogProfile: showSendDialogProfile
   };
 
 });
